8b6797afa1be735efd7eb0adfb9dba0337bca4c5
ziheng <zihenglv@gmail.com>
2019-02-07 02:13:12 +0800

    [update]: src/image_type_detector.sh

--- src/image_type_detector.sh
+++ src/image_type_detector.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
-# -------------------------------------------------
-# Simple Bash Script for Detecting Images File Type
-# -------------------------------------------------
+# ----------------------------
+# Simple bash script for
+# detecting images file type.
+# ----------------------------
 # Author: zihengCat
-# Requirements: `bash`, `xxd`
+# Requirements: bash, xxd
 # Platform: UNIX, Linux, macOS
-# -------------------------------------------------
+# ----------------------------
 if [[ ${#} != 1 ]]
 then
     echo "[ERROR]: command line parameter does not fit"
 else
-    # Read first 3 bytes and first 4 bytes
-    # ================
-    # JPEG => FFD8FF
-    # PNG  => 89504E47
-    # GIF  => 47494638
+    # ===================================
+    # Read first 3 bytes or first 4 bytes
+    # ===================================
+    # JPEG -> FFD8FF
+    # PNG  -> 89504E47
+    # GIF  -> 47494638
     # ================
     bytes_3=$(xxd -p -l 3 ${1})
     bytes_4=$(xxd -p -l 4 ${1})
     if [[ ${bytes_3} == "ffd8ff" ]]
     then
-        echo "[INFO] Image File \`${1}\` Type => \`jpg\`"
+        echo "[INFO] Image File \`${1}\` Type -> \`jpg\`"
     elif [[ ${bytes_4} == "89504e47" ]]
     then
-        echo "[INFO] Image File \`${1}\` Type => \`png\`"
+        echo "[INFO] Image File \`${1}\` Type -> \`png\`"
     elif [[ ${bytes_4} == "47494638" ]]
     then
-        echo "[INFO] Image File \`${1}\` Type => \`gif\`"
+        echo "[INFO] Image File \`${1}\` Type -> \`gif\`"
     else
         echo "[INFO] File \`${1}\` Type => \`unknown\`"
     fi
