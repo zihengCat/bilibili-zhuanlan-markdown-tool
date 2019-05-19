e26608ae97bc19dc88740870a3cc16c872b527c0
ziheng <zihenglv@gmail.com>
2019-05-20 01:26:20 +0800

    [update]: src/BiliColumnCLI.ts

--- dist/BiliColumnCLI.js
+++ dist/BiliColumnCLI.js
@@ -11,8 +11,32 @@ var fs = __importStar(require("fs"));
 var path = __importStar(require("path"));
 var BiliColumnModule = __importStar(require("./BiliColumnMarkdown"));
 var cliArgsArray = process.argv.splice(0);
-if (cliArgsArray.length == 3) {
-    console.log("[INFO]: Sending Markdown files to Bilibili server...");
+if (cliArgsArray.length === 3 &&
+    cliArgsArray[2].startsWith("-")) {
+    switch (cliArgsArray[2]) {
+        case "-h":
+        case "--help":
+            console.log("Bilibili zhuanlan Markdown Tool - CLI Utilities");
+            console.log("Usage:");
+            console.log("\tnode cli.js [options] file.md");
+            console.log("Options:");
+            console.log("\t-h, --help\t\tShow help pages");
+            console.log("\t-v, --version\t\tShow versions");
+            console.log("Examples:");
+            console.log("\tnode cli.js <path/to/your/markdown/file.md>");
+            process.exit(0);
+            break;
+        case "-v":
+        case "--version":
+            console.log("Bilibili zhuanlan Markdown Tool - CLI Utilities");
+            console.log("v1.0.8");
+            process.exit(0);
+            break;
+    }
+}
+else if (cliArgsArray.length === 3 &&
+    !cliArgsArray[2].startsWith("-")) {
+    console.log("[INFO]: Sending markdown files to bilibili server...");
     var uploadsMarkdownFile = function (markdownFilePath) {
         var userConfigFullPath = path.resolve(__dirname, "../config/config.json");
         var userConfig = fs.readFileSync(userConfigFullPath, "utf-8");
@@ -23,4 +47,5 @@ if (cliArgsArray.length == 3) {
 }
 else {
     console.error("[ERROR]: CLI arguments do not fit...");
+    console.error("\t See `--help` pages.");
 }
--- src/BiliColumnCLI.ts
+++ src/BiliColumnCLI.ts
@@ -1,5 +1,5 @@
 /**
- * Bilibili zhuanlan Markdown-Tool - CLI
+ * Bilibili zhuanlan Markdown-Tool - CLI Utilities
  * Author: zihengCat
  * Lincese: MIT
  * GitHub: https://github.com/zihengCat/bilibili-zhuanlan-markdown-tool
@@ -7,25 +7,58 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as BiliColumnModule from "./BiliColumnMarkdown"
-/* 分离命令行参数 */
+/* Splice CLI arguments */
 let cliArgsArray: string[] = process.argv.splice(0);
-if (cliArgsArray.length == 3) {
-    console.log("[INFO]: Sending Markdown files to Bilibili server...");
+/* Show some infomations */
+if (cliArgsArray.length === 3 &&
+    cliArgsArray[2].startsWith("-")) {
+        switch (cliArgsArray[2]) {
+            case "-h":
+            case "--help":
+                console.log("Bilibili zhuanlan Markdown Tool - CLI Utilities");
+                console.log("Usage:");
+                console.log("\tnode cli.js [options] file.md");
+                console.log("Options:");
+                console.log("\t-h, --help\t\tShow help pages");
+                console.log("\t-v, --version\t\tShow versions");
+                console.log("Examples:");
+                console.log("\tnode cli.js <path/to/your/markdown/file.md>");
+                process.exit(0);
+            break
+            case "-v":
+            case "--version":
+                console.log("Bilibili zhuanlan Markdown Tool - CLI Utilities");
+                console.log("v1.0.8");
+                process.exit(0);
+            break
+        }
+/* Do real works */
+} else if (cliArgsArray.length === 3 &&
+          !cliArgsArray[2].startsWith("-")) {
+    console.log("[INFO]: Sending markdown files to bilibili server...");
     let uploadsMarkdownFile: (markdownFilePath: string) => void =
     function(markdownFilePath: string): void {
-        // 读取配置文件
-        let userConfigFullPath: string =
-            path.resolve(__dirname, "../config/config.json");
-        let userConfig = fs.readFileSync(userConfigFullPath, "utf-8");
-        // 调用上传接口
+        /* Read user config file */
+        let userConfigFullPath: string = path.resolve(
+            __dirname,
+            // TODO: Change hardcode `config.json` position.
+            "../config/config.json"
+        );
+        let userConfig = fs.readFileSync(
+            userConfigFullPath, "utf-8"
+        );
+        /* Call `BiliColumnMarkdown` uploads interface */
         let b: BiliColumnModule.BiliColumnMarkdown = 
             new BiliColumnModule.BiliColumnMarkdown();
         b.startProcess(
-            markdownFilePath, // <path/to/your/markdown/file>
+            /* /a/path/to/markdown/file.md */
+            markdownFilePath, 
             JSON.parse(userConfig)
         );
     };
     uploadsMarkdownFile(cliArgsArray[2]);
+/* Report an error */
 } else {
     console.error("[ERROR]: CLI arguments do not fit...");
+    console.error("\t See `--help` pages.");
 }
